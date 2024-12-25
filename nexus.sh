8d792f8ea532258186e84b2f9c9bcd420259a139
FakeMichau <49685661+FakeMichau@users.noreply.github.com>
2024-12-26 00:50:40 +0100

    Make the nexus package have the license

--- nexus.sh
+++ nexus.sh
@@ -3,7 +3,7 @@
 source PKGBUILD
 rm -rf nexus
 mkdir nexus && cd nexus || exit 1
-cp ../fgmod.sh ../fgmod-uninstaller.sh ../prepare.sh .
+cp ../fgmod.sh ../fgmod-uninstaller.sh ../prepare.sh ../LICENSE .
 sed -i 's|standalone=0|standalone=1|g' prepare.sh
-7z a -t7z "../fgmod $pkgver.7z" fgmod.sh fgmod-uninstaller.sh prepare.sh
+7z a -t7z "../fgmod $pkgver.7z" fgmod.sh fgmod-uninstaller.sh prepare.sh LICENSE
 rm -rf ../nexus
